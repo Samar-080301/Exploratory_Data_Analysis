08a93fb903dc82ecedd0d60124c4502384169359
Samar Pratap Singh <35553535+Samar-080301@users.noreply.github.com>
2020-10-06 15:53:45 +0530

    Update eda.py

--- eda.py
+++ eda.py
@@ -1,11 +1,19 @@
 import pandas as pd 
+import numpy as np
 from sklearn import preprocessing
 df = pd.read_csv(r'train.csv',index_col='Id')
+print(df.shape)
 df.head()
+colsNum = df.select_dtypes(np.number).columns
+colsObj = df.columns.difference(colsNum)
+
+df[colsNum] = df[colsNum].fillna(df[colsNum].mean()//1)
+df[colsObj] = df[colsObj].fillna(df[colsObj].mode().iloc[0])
+
 label_encoder = preprocessing.LabelEncoder() 
-for col in df.columns:
-      
-    if df[col].dtype == 'O':
-        df[col] = label_encoder.fit_transform(df[col])
-    print(col) #to check the malacious column
-print(df) 
+for col in colsObj:
+    df[col] = label_encoder.fit_transform(df[col])
+df.head()
+for col in colsObj:
+    df[col] = label_encoder.inverse_transform(df[col])
+df.head()
